4014ab672932c12265202f65cc7b8c52170ff5fb
Brian Jenkins <bonkydog@bonkydog.com>
2014-07-08 11:08:01 -0700

    Configure using config/isimud.yml in railtie.

--- lib/isimud/railtie.rb
+++ lib/isimud/railtie.rb
@@ -3,7 +3,15 @@ require 'rails'
 module Isimud
   class Railtie < Rails::Railtie
     initializer 'isimud.configure' do |app|
-      
+      require 'erb'
+      configs = YAML::load(ERB.new(IO.read(Rails.root.join('config/isimud.yml'))).result).stringify_keys
+      config = configs[Rails.env].stringify_keys
+      app.config.isimud = case config['client']
+      when 'bunny'
+        Isimud::BunnyClient.new(config['broker_url'])
+      when 'test'
+        Isimud::TestClient.new
+      end
     end
   end
 end
