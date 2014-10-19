147a7090f11fe213ccb280415e3bc87466cecec5
George Feil <george.feil@keas.com>
2014-10-19 16:42:10 -0700

    added documentation

--- lib/isimud.rb
+++ lib/isimud.rb
@@ -11,6 +11,26 @@ require 'isimud/version'
 module Isimud
   include ::ActiveSupport::Configurable
 
+  # @!attribute [rw] client_type
+  #   @return [Enumerable<'bunny', 'test'>] Type of client to use
+  # @!attribute [rw] client_options
+  #   @return [Hash] client specific options
+  # @!attribute [rw] default_client
+  #   @return [Isimud::Client] default client
+  # @!attribute [rw] enable_model_watcher
+  #   @return [Boolean] when set, send Isimud::ModelWatcher events
+  # @!attribute [rw] logger
+  #   @return [Logger] logger for tracing messages (Rails.logger)
+  # @!attribute [rw] log_level
+  #   @return [Symbol] log level (:debug)
+  # @!attribute [rw] model_watcher_schema
+  #   @return [String] schema name (Rails.configuration.database_configuration[Rails.env]['database'])
+  # @!attribute [rw] prefetch_count
+  #   @return [Integer] number of messages to fetch -- only applies to BunnyClient
+  # @!attribute [rw] retry_failures
+  #   @return [Boolean] when set, if an exception occurs during message processing, requeue it
+  # @!attribute [rw] server
+  #   @return [<String, Hash>] server connection attributes
   config_accessor :client_type, :client_options, :default_client, :enable_model_watcher, :logger, :log_level,
                   :model_watcher_schema, :model_watcher_exchange, :prefetch_count, :retry_failures, :server
 
@@ -19,18 +39,23 @@ module Isimud
     "Isimud::#{type}".constantize
   end
 
+  # Fetch or initialize the messaging client for this process.
+  # @return [Isimud::Client] messaging client
   def self.client
     self.default_client ||= client_class.new(server, client_options || {})
   end
 
+  # Connect to the messaging server
   def self.connect
     client.connect
   end
 
+  # Return status of model watching mode
   def self.model_watcher_enabled?
     enable_model_watcher.nil? || enable_model_watcher
   end
 
+  # Reconnect the messaging client
   def self.reconnect
     client.reconnect
   end
--- lib/isimud/client.rb
+++ lib/isimud/client.rb
@@ -1,6 +1,38 @@
 module Isimud
+  # @abstract Messaging queue service client
   class Client
     include Isimud::Logging
+
+    def initialize(server = nil, options = nil)
+    end
+
+    def connect
+    end
+
+    def channel
+    end
+
+    def connected?
+    end
+
+    def close
+    end
+
+    def bind(queue_name, exchange_name, *keys, &method)
+    end
+
+    def publish(exchange, routing_key, payload)
+    end
+
+    def reset
+    end
+
+    def reconnect
+    end
+
+    def logger
+      Isimud.logger
+    end
   end
 end
 
